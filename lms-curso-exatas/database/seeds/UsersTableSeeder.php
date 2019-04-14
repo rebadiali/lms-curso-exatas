032f16152165e3b5eda9b8ee379d50529edfe09d
Renata <renata.badiali@gmail.com>
2019-04-14 20:38:02 -0300

    Adicionar um usuário de cada tipo para exemplo

--- lms-curso-exatas/database/seeds/UsersTableSeeder.php
+++ lms-curso-exatas/database/seeds/UsersTableSeeder.php
@@ -13,15 +13,35 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table("users")->insert(
-            array(
-                'id' => 1,
-                'user_type' => User::ADMIN,
-                'name' => "Renata",
-                'email' => "rb@gmail.com",
-                'password' => Hash::make(123456),
-                'created_at' => now(),
-                'updated_at' => now(),
-            )
+            [
+                [
+                    'id' => 1,
+                    'user_type' => User::ADMIN,
+                    'name' => "Admin",
+                    'email' => "admin@admin.com",
+                    'password' => Hash::make(123456),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+                [
+                    'id' => 2,
+                    'user_type' => User::PROFESSOR,
+                    'name' => "Professor",
+                    'email' => "professor@professor.com",
+                    'password' => Hash::make(123456),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+                [
+                    'id' => 3,
+                    'user_type' => User::ALUNO,
+                    'name' => "Aluno",
+                    'email' => "aluno@aluno.com",
+                    'password' => Hash::make(123456),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+            ]
         );
     }
 }
