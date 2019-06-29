448481fef75dda3841b821d7e4570f5675bdde99
Hálex Abreu <mse_habreu@uolinc.com>
2019-06-29 16:19:29 -0300

    adicionando botão de Deslogar no menu

--- lms-fe/src/views/Dashboard.vue
+++ lms-fe/src/views/Dashboard.vue
@@ -7,16 +7,19 @@
           <b-nav-item href="/cadastro-tema">Tema</b-nav-item>
 
           <!-- Navbar dropdowns -->
-          <b-nav-item-dropdown text="Cursos" right>
+          <b-nav-item-dropdown text="Cursos">
             <b-dropdown-item to="/lista-de-cursos">Cursos</b-dropdown-item>
             <b-dropdown-item to="/cadastro-curso">Cadastro de Curso</b-dropdown-item>
           </b-nav-item-dropdown>
 
-          <b-nav-item-dropdown text="Questões" right>
+          <b-nav-item-dropdown text="Questões">
             <b-dropdown-item to="/cadastro-questao">Cadastro de Questões</b-dropdown-item>
             <b-dropdown-item to="/cadastro-alternativas">Alternativas</b-dropdown-item>
             <b-dropdown-item to="/associacao_quest_tema">Associar Tema e Questão</b-dropdown-item>
           </b-nav-item-dropdown>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item @click="deslogarUser">Deslogar</b-nav-item>
         </b-navbar-nav>
       </b-navbar>
     </div>
@@ -32,6 +35,13 @@
       }
     },
     methods: {
+      deslogarUser: function(){
+        sessionStorage.removeItem("idUser");
+        sessionStorage.removeItem("userName");
+        sessionStorage.removeItem("typeUser");
+        sessionStorage.removeItem("tokenUser");
+        this.$router.push('/');
+      }
     }
   }
 </script>
